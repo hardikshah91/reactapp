bc53a0437e0cbb49854c2198c62aeb9eae324c3c
hardikshah91 <hardik.4546@gmail.com>
2017-04-13 18:25:40 +0530

    Commit 4

--- App.jsx
+++ App.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import Header from './components/Header.jsx';
+import Content from './components/Content.jsx';
+import Main_banner from './components/Main_banner.jsx';
+import Footer from './components/Footer.jsx';
 
 class App extends React.Component {
    render() {
@@ -13,4 +17,5 @@ class App extends React.Component {
       );
    }
 }
+
 export default App;
--- components/Content.jsx
+++ components/Content.jsx
@@ -1,28 +1,30 @@
+import React from 'react';
+
 class Content extends React.Component {
    render() {
       return (
          <div>
+               <div className="wrapper">
+                  <div className="contain white">
+                     <div className="compre-card">
+                        <img src="img/img_compare cards.png" alt="compare cards"/>
+                        <p>Compare cards</p>
+                     </div>
 
-            <div className="wrapper">
-               <div className="contain white">
-                  <div className="compre-card">
-                     <img src="img/img_compare cards.png" alt="compare cards"/>
-                     <p>Compare cards</p>
-                  </div>
+                     <div className="get-card">
+                        <img src="img/img_get a card.png" alt="get a card"/>
+                        <p>Get a card</p>
+                     </div>
 
-                  <div className="get-card">
-                     <img src="img/img_get a card.png" alt="get a card"/>
-                     <p>Get a card</p>
-                  </div>
-
-                  <div className="user-review">
-                     <img src="img/img_user reviews.png" alt="user reviews"/>
-                     <p>User reviews</p>
+                     <div className="user-review">
+                        <img src="img/img_user reviews.png" alt="user reviews"/>
+                        <p>User reviews</p>
+                     </div>
                   </div>
                </div>
-            </div>
-
          </div>
       );
    }
 }
+
+export default Content;
